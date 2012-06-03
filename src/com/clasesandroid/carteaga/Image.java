71e8ddb66176bdc8ba09b5bd5a52d37ddaa767c8
Cristian Arteaga <carteaga19@gmail.com>
2012-06-02 22:53:41 -0400

    Se agregan urls

--- src/com/clasesandroid/carteaga/Image.java
+++ src/com/clasesandroid/carteaga/Image.java
@@ -3,6 +3,7 @@ package com.clasesandroid.carteaga;
 public class Image {
 	
 	public static String[] images = {
-		"",
+		"http://ttkv-hameln-pyrmont.de/content/newsbilder/News_Auto.jpg",
+		"http://www.android-spa.com/templates/cleanSilver/images/logoAndroid_2.png",
 	};
 }
